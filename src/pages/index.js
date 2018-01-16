33546830937d96b385b3fc5a488928c544579bb7
Axolotl <samuelmachat@gmail.com>
2018-01-15 22:58:12 -0500

    first-commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+// also known as the home page
+
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div class="content">
+    <div class="field field-name-field-page-image field-type-image field-label-hidden"><div class="field-items"><div class="field-item even"><img typeof="foaf:Image" src="http://storyofkeswick.com/sites/default/files/styles/page_header/public/R-01-3.jpg" alt="The Story of Keswick Hall" title="The Story of Keswick Hall" /></div></div></div><div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Since The Story of Keswick Hall was published in the spring of 2011 and updated and reprinted in 2014, it has been the subject of numerous presentations in and around Charlottesville, as well as a <a target="_blank" href="http://www.dailyprogress.com/entertainment/author-documents-keswick-hall-s-history/article_2f5b93b1-aaf5-515d-a323-2647968bd8ce.html?mode=jqm">feature story on August 14, 2011 in the <i>Charlottesville Daily Progress</i> by David Mauer</a>, a selection at the <a target="_blank" href="http://www.cvillepodcast.com/2012/03/23/virginia-festival-of-the-book-if-buildings-could-talk/">2012 Virginia Festival of the Book</a> , a 2012 non-fiction nominee by the Library of Virginia, a segment on PBS's <a target="_blank" href="http://video.pbs.org/video/2365219032/">Charlottesville Inside Out on April 3, 2014, with host Terri Allard</a>  and the subject of a May 23, 2015, broadcast on <a target="_blank" href="https://soundcloud.com/1070wina/agl-5-23-15-patricia-castelli">  Charlottesville's WINA radio program <i>A Gracious Life</i> with Jeanne McCusker</a>.
+      </p><p>Bob Crawford could not have had any idea how prominent his house would become: Villa Crawford, built in 1912, served its first 35 years as the private residence of five different owners, then took a 42-year roller coaster ride as a country club with ups and downs of both membership and drama, and since 1993 has been the historic wing of Keswick Hall, a world-class resort that draws visitors from around the world.
+      </p><p></p><p>The Story of Keswick Hall is a book that weaves first hand accounts and archival documentation together with more than 150 images and photographs (many never before published) courtesy of current and former club members, staff members, and local residents. In it, residents and visitors have a snapshot into the storied past and exciting new days of this extraordinary property. History, of course, continues to reveal itself, and new stories about former residents entertain audiences frequently. To make arrangements for a tour or presentation, please contact Patricia by <a href="mailto:papershoepress@gmail.com?subject=Keswick%20Inquiry">email</a>.</p>
+      <p>Here, on <a href="http://www.storyofkeswick.com">storyofkeswick.com</a>, <a href="/purchase">order your own copy</a>, and/or <a href="/node/add/historical-content">add to the ongoing conversation</a> (add your own memories, stories, images, photographs, etc.), and/or <a href="http://www.keswick.com">make reservations</a> to come visit so you can walk old paths and make new memories.</p>
+      <p>Maybe you were there in 1969 when Arthur Ashe played, or in 1992 when Arnold Palmer helped celebrate the opening of the redesigned golf course. Maybe you remember a conversation with B.A. or Knox Turnbull or Donald Stevens. Maybe you ate in the Hunt Room or played on the golf course when it was only nine holes. Maybe the chefs let you in the kitchen and gave you tastes of upcoming dinners or snacks on the side. Maybe you remember the horses grazing in Liz Wheeler’s corral, or the bubble over the tennis courts, or Debbie Slee’s dress shop. Maybe you attended a wedding, a golf tournament, a fall festival or a Sunday afternoon tea dance.</p>
+      <p></p>
+      <ul>We encourage you 
+      <li>to applaud an individual who made a difference for you, </li>
+      <li>to ask a question (such as Does anyone know where the Broad Oak house stood?), </li>
+      <li>to answer a question (such as what happened to the 99 marble columns), </li>
+      <li>to relate an unforgettable experience, </li>
+      <li>to give more detail because you were there, </li>
+      <li>to corroborate a story, </li>
+      <li>to explain why you came, </li>
+      <li>to describe the food or the setting as you experienced it, </li>
+      <li>to share a photo taken on the property, </li>
+      <li>to provide text from a brochure, advertisement, or other printed material, </li>
+      <li>etc. </li>
+      </ul><p>
+      </p><p>Just click on <a href="/ongoing-conversation">An Ongoing Conversation: More Stories and Images</a> and find the time frame of your memories. (These sections correspond to the chapters in the book.) Read through some of what others have contributed, and if you want, look for <a href="/node/add/historical-content">Add Your Own</a> and follow the prompts.</p>
+      </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
