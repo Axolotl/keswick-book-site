5d9b61954290444ed91f70e313efc346d9f022ed
Axolotl <samuelmachat@gmail.com>
2018-01-18 15:14:00 -0500

    added static mock of ongoing-conversation w/ existing styles

---
+++ src/pages/ongoing-conversation.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const OngoingConversation = () => (
+  <div className="content node-type-historical-content">
+    <div className="view view-ongoing-conversation view-id-ongoing_conversation view-display-id-page view-dom-id-1">
+      <div className="view-content">
+
+
+        <div className="views-row views-row-1 views-row-odd views-row-first">
+          <div id="node-34" className="node node-historical-content node-teaser clearfix" about="/content/gregorian-chants" typeof="sioc:Item foaf:Document">
+            <h2 property="dc:title" datatype="">
+              <a href="/content/gregorian-chants">Gregorian chants</a>
+            </h2>
+            <div className="content">
+              <div className="field field-name-field-timeframe field-type-list-text field-label-hidden">
+                <div className="field-items">
+                  <div className="field-item even">1990-1999</div>
+                </div>
+              </div>
+              <div className="field field-name-body field-type-text-with-summary field-label-hidden">
+                <div className="field-items">
+                  <div className="field-item even" property="content:encoded">
+                    <p>Throughout the Ashley years, every Sunday from 7 a.m. till noon, guests who wandered into the snooker room heard Gregorian chants. This was what B.A. wanted -- it was an absolute, no question (we did it every Sunday), and shows his cultural background. A steamer trunk sat in the snooker room which housed a stereo system, and we (the porters) set up the cd's in the system so the chants would be heard.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <ul className="links inline">
+              <li className="node-readmore first last">
+                <a href="/content/gregorian-chants" rel="tag" title="Gregorian chants">Read more<span className="element-invisible"> about Gregorian chants</span></a>
+              </li>
+            </ul>
+
+          </div>  
+        </div>
+
+
+        <div className="views-row views-row-2 views-row-even">
+          <div id="node-33" className="node node-historical-content node-teaser clearfix" about="/content/last-supper" typeof="sioc:Item foaf:Document">
+            <h2 property="dc:title" datatype="">
+              <a href="/content/last-supper">The Last Supper!</a>
+            </h2>
+            <div className="content">
+              <div className="field field-name-field-timeframe field-type-list-text field-label-hidden">
+                <div className="field-items">
+                  <div className="field-item even">1972-1990</div>
+                </div>
+              </div>
+              <div className="field field-name-body field-type-text-with-summary field-label-hidden">
+                <div className="field-items">
+                  <div className="field-item even" property="content:encoded">
+                    <p>In 1978, UVA invited a Professor of Ethics to spend a year teaching in the School of Religion. He and his wife were provided a membership at the country club. They invited my husband Herb and me to go to An Oyster Bash. Oysters were in every dish - peas, potatoes, etc. It was delicious! We learned this was the last night a supper would be served at the club. How sad!<br />
+                    Kitty Pickett</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <ul className="links inline">
+              <li className="node-readmore first last">
+                <a href="/content/last-supper" rel="tag" title="The Last Supper!">Read more<span className="element-invisible"> about The Last Supper!</span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default OngoingConversation
