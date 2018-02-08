4ff12c86381714ecb0029af68e0adf49e5434887
Drew Machat <dmachat@gmail.com>
2018-02-07 23:03:10 -0800

    Fix pagination
    - Remove `/pages/ongoing-conversation.js`
    - Use pathPrefix prop to let `createPaginatedPages` generate the pages
    at that path
    - Break up a few components

--- src/templates/template.js
+++ src/templates/template.js
@@ -1,46 +1,6 @@
 import React from 'react';
-//import Helmet from 'react-helmet';
-// import '../css/blog-post.css';
 
-import Link from 'gatsby-link';
-
-
-const NavLink = props => {
-  if (!props.test) {
-    return <Link to={props.url} className="pagination-item">{props.text}</Link>;
-  } else {
-    return <span className="pagination-item">{props.text}</span>;
-  }
-};
-
-export default ({data, pathContext}) => {
-  const { group, index, first, last } = pathContext;
-  const previousUrl = index - 1 == 1 ? "" : (index - 1).toString();
-  const nextUrl = (index + 1).toString();
-
-  return (
-    <div>
-      {group.map(({ node }, idx) => (
-        <div className="post" key={idx}>
-          <Link to={node.fields.slug} className="post-title">
-            {node.fields.title}
-          </Link>
-          <span className="post-date">{node.fields.date}</span>
-          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-          <Link to={node.fields.slug}>Read More ></Link>
-        </div>
-      ))}
-      <div className="pagination">
-        <NavLink test={last} url={nextUrl} text="Older" />
-        <NavLink test={first} url={previousUrl} text="Newer" />
-      </div>
-    </div>
-  );
-}
-
-
-
-/*export default function Template({ data }) {
+export default function Template({ data }) {
   const { markdownRemark: post } = data;
   return (
     <div className="content node-type-historical-content">
@@ -66,8 +26,6 @@ export default ({data, pathContext}) => {
     </div>
   );
 }
-
-*/
 
 export const pageQuery = graphql`
   query PostByPath($path: String!) {
