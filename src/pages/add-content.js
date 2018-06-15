80d29a44e10690411732d1dc7cd0107177299b3d
Axolotl <samuelmachat@gmail.com>
2018-06-14 17:09:20 -0700

    prettierified codebase

--- src/pages/add-content.js
+++ src/pages/add-content.js
@@ -5,9 +5,14 @@ const AddContent = () => (
   <div className="field-name-body">
     <h2 className="add-story-title">Add your story!</h2>
     <br />
-    <p>We invite you to add to the conversation by sending us your story and a picture. To do so, email us at papershoepress@gmail.com. We will review your story and get it up on the site as soon as possible!</p>
+    <p>
+      We invite you to add to the conversation by sending us your story and a
+      picture. To do so, email us at papershoepress@gmail.com. We will review
+      your story and get it up on the site as soon as possible!
+    </p>
 
-    <ul>Remember to include the following in your message:
+    <ul>
+      Remember to include the following in your message:
       <li>an intriguing title ... </li>
       <li>the year it takes place </li>
       <li>your name </li>
